46adaff4248876c6e56e565b26856b95e147a56f
biligto6242 <biligto6242@yandex.ru>
2018-05-09 21:10:01 +0300

    Lesson 1.3

--- index.php
+++ index.php
@@ -1,41 +1,63 @@
 <?php
 $continents = [
-    'Eurasia' => ['Panthera tigris' ,'Mustela erminea' ,'Mellivora capensis'],
-    'Africa' => ['Loxodonta africana' , 'Hippopotamus amphibius' ,'Gorilla'],
-    'NorthAmerica' => ['Alligator mississippiensis' ,'Grizzly'],
-    'SouthAmerica' => ['Eunectes murinus' ,'Pygocentrus nattereri'],
-    'Australia' => ['Macropus' ,'Ornithorhynchus anatinus'],
-    'Antarctica' => ['Spheniscidae'],
+    'Eurasia' => ['Panthera tigris', 'Mustela erminea', 'Mellivora capensis'],
+    'Africa' => ['Loxodonta africana', 'Hippopotamus amphibius', 'Gorilla', 'Orycteropus afer'],
+    'NorthAmerica' => ['Alligator mississippiensis', 'Grizzly', 'Gulo gulo'],
+    'SouthAmerica' => ['Eunectes murinus', 'Pygocentrus nattereri', 'Panthera onca'],
+    'Australia' => ['Macropus', 'Ornithorhynchus anatinus', 'Cacatuidae'],
+    'Antarctica' => ['Spheniscidae', 'Leptonychotes weddellii', 'Aptenodytes forsteri'],
 ];
+
 $animals2 = [];
 $first = [];
 $second = [];
+$fantasy_animals = [];
 
 foreach ($continents as $continent => $animalcontinent) {
     foreach ($animalcontinent as $animals) {
-        $count = substr_count($animals, ' ');
-        if ($count === 1) {
-            $animals2[] = $animals;
+        if (str_word_count($animals)==2) {
+            $animals2[$continent][] = $animals;
+            $counts = explode(' ', $animals);
+            $first[$continent][] = $counts[0];
+            $second[] = $counts[1];
         }
     }
 }
-echo 'Массив зверей название, которых состоит из 2-х слов:';
-var_dump($animals2);
 
-foreach($animals2 as $name){
-    $parts = explode(' ', $name);
-    $first[] = $parts[0];
-    $second[] = $parts[1];
-}
-
-$fantasy_animals= [];
-
-echo '<hr>';
 shuffle($second);
-echo "Массив из выдуманных животных:";
-echo '<hr>';
-foreach ($second as $last) {
-    $first1 = array_shift($first);
-    echo $first1 . ' ' . $last . '<br>';
+
+$i = 0;
+foreach ($first as $continent => $animalcontinent) {
+    foreach ($animalcontinent as $animals) {
+        $fantasy_animals[$continent][] = $animals.' '.$second[$i];
+        $i++;
+    }
 }
+
 ?>
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Домашнее задание к лекции 1.3 «Строки, массивы и объекты»</title>
+</head>
+<body>
+<?php
+    echo '<h1>Массив животных</h1>';
+    foreach ($continents as $continent => $animalcontinent) {
+        $animals = implode(', ', $animalcontinent);
+        echo "<h2>$continent</h2><p>$animals</p>";
+    }
+    echo '<hr>';
+    echo '<h1>Массив выдуманных животных</h1>';
+    //выводим результат
+    foreach ($fantasy_animals as $continent => $animalcontinent) {
+        $new_animals = implode(', ', $animalcontinent);
+        echo "<h2>$continent</h2><p>$new_animals</p>";
+    }
+?>
+</body>
+</html>
